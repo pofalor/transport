0200094f80a01b4d0f68a30c7b8a6b6c9715c3aa
Даниил <gromovdk@stud.kai.ru>
2024-12-02 10:23:39 +0300

    Added tree and searching for tree

--- AnalyzePotential.cs
+++ AnalyzePotential.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,19 +19,114 @@ namespace Transport
             return a;
         }
 
-        public static int[][] OptimizeSolution(Element[][] transportPlan)
+        public static int[][] OptimizeSolution(Element[][] transportPlan, int N, int M)
         {
+            ConcurrentBag<Element> bagElements = new ConcurrentBag<Element>();
+
             //TODO: пока я ищу сам ячейку с минимальным значением, в будущем надо будет передавать в метод индекс
-            //transportPlan.fi    
-            var a = new int[1][];
-            return a;
-            //2. Выбираем ячейку с самым маленьким значением
+
+            //Выбираем ячейку с самым маленьким значением
+            Parallel.For(0, N, i => 
+            {
+                Element minElement = new Element()
+                {
+                    Potential = int.MaxValue
+                };
+                for (int j = 0; j < M; j++) 
+                {
+                    var element = transportPlan[i][j];
+                    element.IndexCol = j;
+                    element.IndexRow = i;
+                    if(element.Potential.HasValue && element.Potential <  minElement.Potential)
+                        minElement = element;
+                }
+                bagElements.Add(minElement);
+            });
+
+            var minElem = new Element()
+            {
+                Potential = int.MaxValue
+            };
+            foreach(var element in bagElements)
+            {
+                if(element.Potential.HasValue && element.Potential < minElem.Potential)
+                    minElem = element;
+            }
+
             //3. Цикл перераспределения поставок
+            var allTreeElements = new Dictionary<Element, TreeForOptimize>();
+            var tree = new TreeForOptimize(minElem, transportPlan, allTreeElements, N, M);
+            var allPaths = new List<List<TreeForOptimize>>();
+            var path = new List<TreeForOptimize>();
             // 3.1 Найти путь, по которому можно пройти так, чтобы можно было создать замкнутый круг и двигаться можно только влево, вправо, вверх, вниз
+            GetLongestCyclePathForElement(tree, tree, path, allPaths);
+            var maxLenTreeList = new List<TreeForOptimize>();
+            foreach (var item in allPaths)
+            {
+                if (item.Count > maxLenTreeList.Count)
+                    maxLenTreeList = item;
+            }
             //3.2 После этого переходим к ячейке, которую нужно добавить в решение
+            
+
             //3.3. смотрим две связанные ячейки друг с другм. Берём мин значение из двух. 
             //3.4. В новую ячейку ставим мин. значение
             //3.5. Корректируем значения во всех соответствующих ячейках, при этом если двигаемся вниз или вверх, то корректируем по столбцу. Если влево или вправо, то корректируем по строке.
         }
+
+        public static void GetLongestCyclePathForElement(TreeForOptimize currentElement, TreeForOptimize searchingElement, List<TreeForOptimize> path,
+            List<List<TreeForOptimize>> allPaths)
+        {
+            path.Add(currentElement);
+            var left = currentElement.LeftElement;
+            var right = currentElement.RightElement;
+            var top = currentElement.TopElement;
+            var down = currentElement.DownElement;
+            if (left != null)
+            {
+                if (left != searchingElement)
+                {
+                    GetLongestCyclePathForElement(left, searchingElement, path, allPaths);
+                }
+                else
+                {
+                    allPaths.Add(path);
+                }
+            }
+            if (right != null)
+            {
+                if (right != searchingElement)
+                {
+                    GetLongestCyclePathForElement(right, searchingElement, path, allPaths);
+                }
+                else
+                {
+                    allPaths.Add(path);
+                }
+            }
+            if (top != null)
+            {
+                if (top != searchingElement)
+                {
+                    GetLongestCyclePathForElement(top, searchingElement, path, allPaths);
+                }
+                else
+                {
+                    allPaths.Add(path);
+                }
+            }
+            if (down != null)
+            {
+                if (down != searchingElement)
+                {
+                    GetLongestCyclePathForElement(down, searchingElement, path, allPaths);
+                }
+                else
+                {
+                    allPaths.Add(path);
+                }
+            }
+        }
     }
 }
+
---
+++ TreeForOptimize.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    public class TreeForOptimize
+    {
+        public TreeForOptimize(Element currentElement, Element[][] transportPlan, Dictionary<Element, TreeForOptimize> allTreeElements, int N, int M)
+        {
+            CurrentElement = currentElement;
+
+            allTreeElements[currentElement] = this;
+
+            var downElement = N > currentElement.IndexRow + 1 ? GetElementWithOffset(transportPlan, currentElement.IndexRow, currentElement.IndexCol, 1) : null;
+            if (downElement != null)
+            {
+                DownElement = allTreeElements.Get(downElement, new TreeForOptimize(downElement, transportPlan, allTreeElements, N, M));
+            }
+
+            var topElement = N - 1 >= 0 ? GetElementWithOffset(transportPlan, currentElement.IndexRow, currentElement.IndexCol, -1) : null;
+            if (topElement != null)
+            {
+                TopElement = allTreeElements.Get(topElement, new TreeForOptimize(topElement, transportPlan, allTreeElements, N, M));
+            }
+
+            var leftElement = currentElement.IndexCol - 1 >= 0 ? GetElementWithOffset(transportPlan, currentElement.IndexRow, currentElement.IndexCol, offsetCol: -1) : null;
+            if (leftElement != null)
+            {
+                LeftElement = allTreeElements.Get(leftElement, new TreeForOptimize(leftElement, transportPlan, allTreeElements, N, M));
+            }
+
+            var rightElement = M > currentElement.IndexCol + 1 ? GetElementWithOffset(transportPlan, currentElement.IndexRow, currentElement.IndexCol, offsetCol: 1) : null;
+            if (rightElement != null)
+            {
+                RightElement = allTreeElements.Get(rightElement, new TreeForOptimize(rightElement, transportPlan, allTreeElements, N, M));
+            }
+        }
+
+        public Element CurrentElement { get; set; }
+
+        public TreeForOptimize? DownElement { get; set; } = null;
+
+        public TreeForOptimize? TopElement { get; set; } = null;
+
+        public TreeForOptimize? LeftElement { get; set; } = null;
+
+        public TreeForOptimize? RightElement { get; set; } = null;
+
+        private static Element GetElementWithOffset(Element[][] transportPlan, int indexRow, int indexCol, int? offsetRow = null, int? offsetCol = null)
+        {
+            var element = transportPlan[indexRow + offsetRow.GetValueOrDefault(0)][indexCol + offsetCol.GetValueOrDefault(0)];
+            if (!element.Weight.HasValue || element.Weight.Value == -1 || element.Weight.Value == 0)
+            {
+                if (offsetRow.HasValue)
+                {
+                    offsetRow += offsetRow;
+                }
+                if (offsetCol.HasValue)
+                {
+                    offsetCol += offsetCol;
+                }
+                element = GetElementWithOffset(transportPlan, indexRow, indexCol, offsetRow, offsetCol);
+            }
+            return element;
+        }
+
+    }
+
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Получить значение словаря по ключу, в случае отсутствия ключа вернет default
+        /// </summary>
+        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key, TValue defaultValue)
+        {
+            if (source.ContainsKey(key))
+            {
+                return source[key];
+            }
+            return defaultValue;
+        }
+    }
+}
